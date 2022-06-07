0ad36ccdc8de0cfeae0860088a94d096cb1f3d96
EstPain <99329093+EstPain@users.noreply.github.com>
2022-06-07 23:44:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,33 +6,33 @@
 template<typename T, int size>
 class TPQueue {
  private:
-   T Arr[10];
-   int n, k;
+  T Arr[10];
+  int n, k;
  public:
-   TPQueue(): n(0), k(0) {}
-   int Dimension() {
-     return k - n;
-   }
-   void push(T value) {
-     if (Dimension() >= size) {
-       throw std::string("Full!");
-     } else {
-       int totaliser = k - 1;
-       while (totaliser >= n && value.prior > Arr[totaliser % size].prior) {
-         Arr[(totaliser + 1) % size] = Arr[totaliser % size];
-         totaliser--;
-       }
-       Arr[(totaliser + 1) % size] = value;
-       k++;
-     }
-   }
-   T pop() {
-     if (n == k) {
-       throw std::string("Empty!");
-     } else {
-       return Arr[(n++) % size];
-     }
-   }
+  TPQueue(): n(0), k(0) {}
+  int Dimension() {
+    return k - n;
+  }
+  void push(T value) {
+    if (Dimension() >= size) {
+      throw std::string("Full!");
+    } else {
+      int totaliser = k - 1;
+      while (totaliser >= n && value.prior > Arr[totaliser % size].prior) {
+        Arr[(totaliser + 1) % size] = Arr[totaliser % size];
+        totaliser--;
+      }
+      Arr[(totaliser + 1) % size] = value;
+      k++;
+    }
+  }
+  T pop() {
+    if (n == k) {
+      throw std::string("Empty!");
+    } else {
+      return Arr[(n++) % size];
+    }
+  }
 };
 
 struct SYM {
